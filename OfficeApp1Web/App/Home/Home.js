0549bcb3cf4202dbe8a154b2a0c2f5277573e014
Ian Brown <misteribrown@gmail.com>
2016-04-13 14:23:41 +0100

    Add some json

--- OfficeApp1Web/App/Home/Home.js
+++ OfficeApp1Web/App/Home/Home.js
@@ -2,6 +2,27 @@
 
 (function () {
     "use strict";
+    var userDetails = {
+        PI: "E2762956",
+        Name: "Ian Brown",
+        };
+
+    var courses = {
+        Course:  {
+            Title: "L192 Beginners French",
+            Code: "L192",
+            TutorName: "Mrs Tutor",
+            TutorContactEmail: "mrstutor@open.ac.uk",
+            TutorContactVoip: "mrstutor@open.ac.uk",
+            TMAS: [
+                {Title: "TMA01", Url: "https://learn2.open.ac.uk/mod/oucontent/view.php?id=764144"},
+                {Title: "TMA02", Url: "https://learn2.open.ac.uk/mod/oucontent/view.php?id=764174"},
+                {Title: "TMA03", Url: "https://learn2.open.ac.uk/mod/oucontent/view.php?id=764189"},
+                {Title: "TMA04", Url: "https://learn2.open.ac.uk/mod/oucontent/view.php?id=764367"},
+                {Title: "EMA", Url: "https://learn2.open.ac.uk/mod/oucontent/view.php?id=764196"}
+            ]
+        }
+    }
 
     // The initialize function must be run each time a new page is loaded
     Office.initialize = function (reason) {
@@ -9,6 +30,7 @@
             app.initialize();
 
             $('#get-data-from-selection').click(getDataFromSelection);
+            $('#insert-standard-header').click(insertStandardHeader);
         });
     };
 
@@ -23,5 +45,10 @@
                 }
             }
         );
+    }
+
+    function insertStandardHeader() {
+      //  Office.context.document.
+
     }
 })();

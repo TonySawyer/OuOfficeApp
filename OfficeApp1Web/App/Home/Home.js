979f0d500a1d8a8887d04698af732e67ee8618db
Ian Brown <misteribrown@gmail.com>
2016-04-15 12:13:44 +0100

    Can add a header!

--- OfficeApp1Web/App/Home/Home.js
+++ OfficeApp1Web/App/Home/Home.js
@@ -48,7 +48,89 @@
     }
 
     function insertStandardHeader() {
-      //  Office.context.document.
+        // Run a batch operation against the Word object model.
+        Word.run(function (context) {
 
+            // Create a proxy sectionsCollection object.
+            var mySections = context.document.sections;
+
+            // Queue a commmand to load the sections.
+            context.load(mySections, 'body/style');
+
+            // Synchronize the document state by executing the queued-up commands, 
+            // and return a promise to indicate task completion.
+            return context.sync().then(function () {
+
+                // Create a proxy object the primary header of the first section. 
+                // Note that the header is a body object.
+                var myHeader = mySections.items[0].getHeader("primary");
+
+                // Queue a command to insert text at the end of the header.
+                myHeader.insertText(getHeaderText(), Word.InsertLocation.end);
+
+                // Queue a command to wrap the header in a content control.
+                myHeader.insertContentControl();
+
+                // Synchronize the document state by executing the queued-up commands, 
+                // and return a promise to indicate task completion.
+                return context.sync().then(function () {
+                    console.log("Added a header to the first section.");
+                });
+            });
+        })
+        .catch(function (error) {
+            console.log('Error: ' + JSON.stringify(error));
+            if (error instanceof OfficeExtension.Error) {
+                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+            }
+        });
+
+
+
+        //var context = Office.context;
+        //var document = context.document;
+        //var sections = document.sections;
+        //context.load(sections, 'body/style');
+        //context.sync().then(function () {
+        //    var header = sections.items[0].getHeader("primary");
+        //    header.insertText(getHeaderText(), Word.InsertLocation.end);
+        //    header.insertContentControl();
+        //    contex.sync();
+
+        //});
+
+        //displayAllBindings();
+        //if (Office.context.document.setSelectedDataAsync) {
+        //    //Office.context.document.goToByIdAsync
+            
+        //    Office.context.document.setSelectedDataAsync(getHeaderText(), function (result) {
+        //        //Upon return, if the call was unable to insert text, let the user know.
+        //        if (result.status === Office.AsyncResultStatus.Failed) {
+        //            app.showNotification("There's a problem!", "The sample text was unable to be inserted.");
+        //        }
+        //    });
+        //} else {
+        //    app.showNotification("There's a problem!", "This product does not support inserting content.");
+        //}
+    }
+
+    function displayAllBindings() {
+        write("getting bindings");
+        Office.context.document.bindings.getAllAsync(function (asyncResult) {
+            var bindingString = '';
+            for (var i in asyncResult.value) {
+                bindingString += asyncResult.value[i].id + '\n';
+            }
+            write('Existing bindings: ' + bindingString);
+        });
+    }
+
+    // Function that writes to a div with id='message' on the page.
+    function write(message) {
+        document.getElementById('message').innerText += message;
+    }
+
+    function getHeaderText() {
+        return "Ian Brown (PI E2762956) L192 ETMA01";
     }
 })();
